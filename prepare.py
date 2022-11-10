9c16d1c08824a19d70e1b1a23caf9bcb958ce4a6
Maria Karpei <maria.karpei93@gmail.com>
2022-11-11 00:32:57 +0100

    Leap year

--- prepare.py
+++ prepare.py
@@ -1,1 +1,21 @@
+"""Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
+Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function."""
+ # Leap year
+# The year can be evenly divided by 4, is a leap year, unless:
+# The year can be evenly divided by 100, it is NOT a leap year, unless:
+# The year is also evenly divisible by 400. Then it is a leap year.
+def is_leap(year):
+    leap = False
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+    return False
 
+year = int(input())
+print(is_leap(year))
+###### Second solution #######
+def is_leap(year):
+    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
+year = int(input())
+print(is_leap(year))
