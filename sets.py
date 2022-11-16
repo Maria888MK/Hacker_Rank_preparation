ddd083998a0656deaade93395b249af7e3d0c95d
Maria Karpei <maria.karpei93@gmail.com>
2022-11-16 11:43:14 +0100

    Check Subset

--- sets.py
+++ sets.py
@@ -256,3 +256,33 @@ if sum(result) == n:
     print(True)
 else:
     print(False)
+####################################### Check Subset ######################################
+"""
+You are given two sets, A and B .
+Your job is to find whether set A is a subset of set  B.
+input:
+3
+5
+1 2 3 5 6
+9
+9 8 5 6 3 2 1 4 7
+1
+2
+5
+3 6 5 4 1
+7
+1 2 3 5 6 8 9
+3
+9 8 2
+output:
+True 
+False
+False
+"""
+T = int(input())
+for i in range(T):
+    lenght_A = int(input())
+    A = set(map(int, input().split()))
+    lenght_B = int(input())
+    B = set(map(int, input().split()))
+    print(A.issubset(B))
