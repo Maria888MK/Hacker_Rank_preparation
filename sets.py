a95d6e774c77a146524f77fb3864a6bfd2619e73
Maria Karpei <maria.karpei93@gmail.com>
2022-11-16 10:42:26 +0100

    Check Superset

--- sets.py
+++ sets.py
@@ -229,4 +229,30 @@ try:
     
     
 except EOFError as e:
-    print(end="")    
+    print(end="")
+####################################### Check Strict Superset ######################################
+""" 
+You are given a set  and  other sets.
+Your job is to find whether set  is a strict superset of each of the  sets.
+Print True, if  is a strict superset of each of the  sets. Otherwise, print False.
+A strict superset has at least one element that does not exist in its subset.
+input: 
+1 2 3 4 5 6 7 8 9 10 11 12 23 45 84 78
+2
+1 2 3 4 5
+100 11 12
+output: False
+"""
+A = set(map(int, input().split()))
+n = int(input())
+result = []
+for _ in range(n):
+    new_set = set(map(int, input().split()))
+    if  A.issuperset(new_set) == True:
+        result.append(True)
+    else:
+        result.append(False)
+if sum(result) == n:
+    print(True)
+else:
+    print(False)
